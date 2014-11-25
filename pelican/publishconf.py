0c8ebc986ebc5ae982fad0b586d996548e484991
Michael V. DePalatis <depalatis@gmail.com>
2014-11-25 21:06:16 +0100

    Reorganization

---
+++ pelican/publishconf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+# This file is only used if you use `make publish` or
+# explicitly specify it as your config file.
+
+import os
+import sys
+sys.path.append(os.curdir)
+from pelicanconf import *
+
+#SITEURL = ''
+
+# TODO: fix this hack. The problem is I want this to be mirrorable on
+# github pages as well as on depalatis.net. But things seem to break
+# then.
+RELATIVE_URLS = True
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+
+DELETE_OUTPUT_DIRECTORY = True
+
+# Following items are often useful when publishing
+
+#DISQUS_SITENAME = ""
+#GOOGLE_ANALYTICS = ""
