0782c009997075f578d087ff88cc14ae1e517512
Michael V. DePalatis <mike@depalatis.net>
2022-07-15 16:29:14 -0600

    Render Markdown

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import asyncio
 from pathlib import Path
 
+from markdown import Markdown
+
 from tornado.log import enable_pretty_logging
 from tornado.web import Application, RequestHandler
 
@@ -8,6 +10,9 @@ SOURCE_DIR = Path(__file__).parent / "src"
 
 
 class PageHandler(RequestHandler):
+    def initialize(self, markdown: Markdown) -> None:
+        self.markdown = markdown
+
     def get(self, path: str) -> None:
         """Translate the input path into a filesystem path; find and write the
         file, rendering Markdown as appropriate.
@@ -32,15 +37,20 @@ class PageHandler(RequestHandler):
         if not path.is_file():
             self.send_error(404)
         else:
-            # TODO: transform Markdown
             # TODO: render template
-            self.write(path.read_bytes())
+            html = self.markdown.convert(path.read_text())
+            self.write(html)
 
 
 async def main() -> None:
     """Run the development server."""
+    markdown = Markdown(
+        extensions=["extra", "codehilite", "meta"], output_format="html5"
+    )
     enable_pretty_logging()
-    app = Application([("(.*)", PageHandler)], debug=True)
+    # FIXME: update regex to only accept .html or ending in /,
+    #        use static handler for everything else
+    app = Application([("(.*)", PageHandler, {"markdown": markdown})], debug=True)
     app.listen(4444, "127.0.0.1")
     await asyncio.Event().wait()
 
