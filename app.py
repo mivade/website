6f5f41243d5cb9760ce81e3f9be032b20ac7780d
Michael V. DePalatis <mike@depalatis.net>
2022-09-13 16:02:56 -0600

    Support generating all static files

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+import argparse
 import asyncio
 from datetime import date
+import itertools
+import os
 from pathlib import Path
+import shutil
 from typing import Any, NewType
 
 from markdown import Markdown
 
+from tornado.httpclient import AsyncHTTPClient
 from tornado.log import enable_pretty_logging
 from tornado.web import Application, RequestHandler, RedirectHandler, StaticFileHandler
 
+DEFAULT_PORT = 4444
 SOURCE_DIR = Path(__file__).parent / "src"
 
 Link = NewType("Link", str)
@@ -80,7 +86,7 @@ class BlogIndexHandler(BaseHandler):
         return sorted(entries, key=lambda entry: entry[0], reverse=True)
 
 
-async def main() -> None:
+async def serve(port: int | None = None) -> None:
     """Run the development server."""
     markdown_kwargs = {
         "extensions": ["extra", "codehilite", "meta"],
@@ -102,12 +108,60 @@ async def main() -> None:
         template_path="templates",
         debug=True,
     )
-    app.listen(4444, "127.0.0.1")
+    app.listen(port or DEFAULT_PORT, "127.0.0.1")
     await asyncio.Event().wait()
 
 
-if __name__ == "__main__":
+async def generate(
+    output_directory: str | os.PathLike | None = "build", port: int | None = None
+) -> None:
+    """Generate all static content."""
+    port = port or DEFAULT_PORT
+    serve_task = asyncio.create_task(serve(port))
+    url_pattern = "http://localhost:{port}/{name}"
+    src = Path("src")
+    dest = Path(output_directory)
+    shutil.rmtree(dest, ignore_errors=True)
+
     try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        pass
+        client = AsyncHTTPClient()
+
+        for path in itertools.chain(src.glob("**/*.*"), [src / "blog" / "index.html"]):
+            name = str(path.relative_to(src))
+            outdir = dest.joinpath(path.relative_to(src).parent)
+            outdir.mkdir(parents=True, exist_ok=True)
+
+            if name.endswith(".md"):
+                name = name.replace(".md", ".html")
+
+            url = url_pattern.format(port=port, name=name)
+            response = await client.fetch(url)
+
+            with dest.joinpath(name).open("wb") as outfile:
+                outfile.write(response.body)
+    finally:
+        serve_task.cancel()
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", "-p", type=int, help="port to serve on")
+    parser.add_argument(
+        "command", choices=["serve", "generate"], help="command to execute"
+    )
+    args = parser.parse_args()
+
+    if args.command == "serve":
+        try:
+            asyncio.run(serve(port=args.port))
+        except KeyboardInterrupt:
+            pass
+    elif args.command == "generate":
+        asyncio.run(generate(port=args.port))
+    else:
+        parser.print_usage()
+        raise SystemExit()
+
+
+if __name__ == "__main__":
+    main()
