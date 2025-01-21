b276843af154f251f20c9dfa655e2e5a8a942239
Michael V. DePalatis <mdepalatis@ascendanalytics.com>
2025-01-21 14:17:17 -0700

    Update submodule and noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,12 @@
 import nox
 
 
-@nox.session()
+@nox.session(venv_backend="none")
 def build(session: nox.Session) -> None:
     """Build the site."""
-    session.run("git", "submodule", "update", external=True)
+    session.run("git", "submodule", "update", "--init", external=True)
+
+    with session.cd("mivade.github.io"):
+        session.run("git", "switch", "main")
+
     session.run("quarto", "render", "src", external=True)
